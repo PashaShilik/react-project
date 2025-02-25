072cbd8303156a848cb96b66f83aff84c77eb074
Ilya <avroro-forever@mail.ru>
2025-02-25 13:36:54 +0300

    create MainSearch component and update SearchBlock

--- src/pages/HomePage/Bloks/SearchBlock/SearchBlock.tsx
+++ src/pages/HomePage/Bloks/SearchBlock/SearchBlock.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import styles from './searchBlock.module.scss'
+import MainSearch from "../../../../components/MainSearch/MainSearch";
+import {Link} from "react-router-dom";
+import {ROUTES} from "../../../../routes/routes";
 
-export const SearchBlock = function() {
-  return (
-    <div className={styles.searchBlock}>
-      Блок для поиска
-    </div>
-  )
+export const SearchBlock = function () {
+    return (
+        <div className={styles.searchBlock}>
+            <MainSearch/>
+            <p className={styles.searchBlock__text}>
+                Can't find something specific?<br/>
+                Use <Link to={ROUTES.search}>advanced search</Link>
+            </p>
+        </div>
+    )
 }
 
