b9d2893380fd5fdc9ab0c0a03bc46683ff14d03d
Ilya <avroro-forever@mail.ru>
2025-02-25 13:35:47 +0300

    create CommonSearch component

---
+++ src/components/Common/CommonSearch/CommonSearch.tsx
@@ -0,0 +1,85 @@
+import React, {FC, useRef, useState} from 'react';
+import styles from './commonSearch.module.scss';
+import defaultImage from '../../../assets/svg/Search.svg'
+import deleteIco from "../../../assets/svg/Delete.svg";
+import loader from '../../../assets/img/loader.webp';
+
+type Props = {
+    value: string;
+    onChangeFn?: (value: string) => void;
+    onFocusFn?: () => void;
+    image?: string;
+    placeholder?: string;
+    maxLength?: number;
+    isLoading?: boolean
+}
+
+const CommonSearch: FC<Props> = ({value, onChangeFn, onFocusFn, image, placeholder, maxLength = 30, isLoading = false}) => {
+    const [inputValue, setInputValue] = useState(value);
+    const [focus, setFocus] = useState(false);
+    const ref = useRef<HTMLInputElement>(null);
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newValue = e.target.value;
+        setInputValue(newValue);
+        if (onChangeFn) {
+            onChangeFn(newValue);
+        }
+    }
+
+    const handleDeleteClick = () => {
+        setInputValue('');
+        if (onChangeFn) {
+            onChangeFn('');
+        }
+    }
+
+    const handleFocus = () => {
+        setFocus(true);
+        if (onFocusFn) {
+            onFocusFn();
+        }
+    }
+
+    const handleBlur = () => {
+        setFocus(false);
+    }
+    const handleWrapperClick = () => {
+        if (ref) {
+            ref.current?.focus();
+        }
+    }
+
+    const loaderOrImage = (isLoading ?
+        <img className={styles.search__loader} src={loader} alt={'loader'}/>
+        :
+        <img className={styles.search__img} src={image || defaultImage} alt={'search'}/>
+    )
+
+    return (
+        <div className={`${styles.wrapper} ${focus ? styles.wrapper_focus : ''}`} onClick={handleWrapperClick}>
+            <div className={styles.search}>
+                {loaderOrImage}
+                <input
+                    className={styles.search__input}
+                    placeholder={placeholder || 'Search...'}
+                    value={value}
+                    maxLength={maxLength}
+                    onChange={handleChange}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                    ref={ref}
+                />
+                {inputValue &&
+                    <img
+                        alt="ico"
+                        src={deleteIco}
+                        className={[styles.search__img, styles.search__img_delete].join(' ')}
+                        onClick={handleDeleteClick}
+                    />
+                }
+            </div>
+        </div>
+    );
+};
+
+export default CommonSearch;
