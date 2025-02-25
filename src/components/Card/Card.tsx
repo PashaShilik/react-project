5ee8e14e147f59f764274c417f35f5dba8489f90
devIggy <devdevgrin@gmail.com>
2025-02-25 10:38:01 +0300

    create Card component and styles

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { IAnime } from "../../types/interfaces/IAnime";
+import styles from "./card.module.scss"
+
+interface CardProps {
+    anime: IAnime
+}
+
+export function Card({ anime }: CardProps) {
+    const navigate = useNavigate();
+
+    const handleClick = (): void => {
+        navigate(`anime/${anime.id}`);
+    };
+
+    const { imageUrl, title, genres, yearStart, yearEnd, description } = anime;
+    const ongoing: string | number = !yearEnd ? "ongoing" : yearEnd; // для аниме, которое еще выпускается
+
+    return (
+        <li
+            className={styles.card}
+            onClick={handleClick}>
+            <img className={styles.card__img} src={imageUrl} alt={title} />
+            <div className={styles.card__info}>
+                <h3 className={styles.card__info_title}>{title}</h3>
+                <span className={styles.card__info_genre}>Genres: {genres}</span>
+                <span className={styles.card__info_year}>Released: {yearStart}</span>
+                <span className={styles.card__info_year}>Ended: {ongoing}</span>
+                <p className={styles.card__info_description}>{description}</p>
+            </div>
+        </li>
+    );
+}
