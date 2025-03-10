570029e431ff095eee9b0108d6b9a9ec24935735
Filimonov <srfilimonoff@gmail.com>
2025-03-10 13:16:59 +0300

    added history icon to header

--- src/components/HeaderAuth/HeaderAuth.tsx
+++ src/components/HeaderAuth/HeaderAuth.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './headerAuth.module.scss';
 
 import favoritesIco from '@/assets/img/favorites.png';
+import historyIco from '@/assets/svg/history-search.svg';
 import userIco from '@/assets/svg/user.svg';
 import { CommonButton } from '@/components/Common/CommonButton/CommonButton';
 import { HeaderBurger } from '@/components/HeaderBurger/HeaderBurger';
@@ -25,6 +26,9 @@ export const HeaderAuth = () => {
   const handleFavoritesClick = () => {
     navigate(ROUTES.favorites);
   };
+  const handleHistoryClick = () => {
+    navigate(ROUTES.history);
+  };
   const handleLogOut = () => {
     localStorage.removeItem(LOCAL_STORAGE_KEYS.AuthMe)
     dispatch(setIsAuth({isAuth:false}))
@@ -43,6 +47,7 @@ export const HeaderAuth = () => {
       <>
         <div className={styles.headerAuth}>
           <img src={favoritesIco} alt="favoritesIco" className={styles.headerAuth__favorites_img} onClick={handleFavoritesClick}/>
+          <img src={historyIco} alt="historySearchIco" className={styles.headerAuth__favorites_img} onClick={handleHistoryClick}/>
           <div className={styles.headerAuth__data_container}>
             <img src={userIco} alt="userIco" className={styles.headerAuth__user_img} />
             <p className={styles.headerAuth__login_text}>@{user.login}</p> 
