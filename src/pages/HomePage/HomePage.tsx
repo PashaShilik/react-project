b24e065e19706b452cd482bd29aac9310eb43c42
pasha_shilik <pasha-shilik@mail.ru>
2025-03-11 14:28:20 +0300

    create introductionBlock in home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import styles from './homePage.module.scss'
-import {SearchBlock} from './Bloks/SearchBlock/SearchBlock';
 import {CardListBlock} from './Bloks/CardListBlock/CardListBlock';
 import { TopSliderBlock } from './Bloks/TopSliderBlock/TopSliderBlock';
 import { ErrorBoundary } from '@/components/ErrorBoundary/ErrorBoundary';
+import IntroductionBlock from './Bloks/IntroductionBlock/IntroductionBlock';
 
 function HomePage() {
   return (
     <div className={styles.homePage}>
+      <IntroductionBlock/>
       <ErrorBoundary>
         <TopSliderBlock/>
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <SearchBlock/>
       </ErrorBoundary>
       <ErrorBoundary>
         <CardListBlock/>
