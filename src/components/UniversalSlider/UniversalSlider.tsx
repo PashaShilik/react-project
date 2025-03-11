88ced81aa9666077f621c3452aca88796a161a3c
pasha_shilik <pasha-shilik@mail.ru>
2025-03-11 15:50:32 +0300

    create slider

--- src/components/UniversalSlider/UniversalSlider.tsx
+++ src/components/UniversalSlider/UniversalSlider.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import styles from "./universalSlider.module.scss";
+import { UniversalData } from "@/types/interfaces/universalData";
+
+import ArrLeft from '@/assets/svg/ArrLeft.svg';
+import ArrRight from '@/assets/svg/ArrRight.svg'
+
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import styles from "./universalSlider.module.scss";
-import { UniversalData } from "@/types/interfaces/universalData";
+import './styles.css';
 
 interface UniversalSliderProps {
   data: UniversalData[];
@@ -37,20 +42,49 @@ export function UniversalSlider({
   pagination = true,
   autoplay = true,
 }: UniversalSliderProps) {
+
+  const swiperRef = useRef<any>(null);
+
+  const handleNext = () => {
+    if (swiperRef.current) {
+      swiperRef.current.swiper.slideNext();
+    }
+  };
+
+  const handlePrev = () => {
+    if (swiperRef.current) {
+      swiperRef.current.swiper.slidePrev(); 
+    }
+  };
+
   return (
-    <Swiper
-      className={styles.slider}
+    <div className={styles.slider}>
+      <Swiper
+      className="mySwiper"
+      ref={swiperRef}
+      loop={true}
       modules={[Navigation, Pagination, Autoplay]}
       spaceBetween={spaceBetween}
       slidesPerView={slidesPerView}
       breakpoints={breakpoints}
-      navigation={navigation}
+      navigation={{
+        nextEl: '.custom-next',
+        prevEl: '.custom-prev',
+      }} 
       pagination={pagination ? { clickable: true } : false}
       autoplay={autoplay ? { delay: 3000, disableOnInteraction: true } : false}
-    >
+      >
       {data.map((item, index) => (
         <SwiperSlide style={{ height: 320 }} key={index}>{renderItem(item)}</SwiperSlide>
       ))}
     </Swiper>
+
+      <div className="custom-prev" onClick={handlePrev}>
+        <img src={ArrLeft} alt="Назад" className={styles.slider__slide_arrow}/>
+      </div>
+      <div className="custom-next" onClick={handleNext}>
+        <img src={ArrRight} alt="Вперед" className={styles.slider__slide_arrow}/>
+      </div>
+    </div>
   );
 }
